e6a5150700bc53f875ce620d5f57fc0de29ea2cc
Marc Teichtahl <teichtah@amazon.com>
2020-12-18 16:28:30 +1100

    chore: refine build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,13 @@
 # Build script is to be run from the root directory !
 
 set -ev
-npm ci
 npm run test:all
 npm run generate:attribution
 npm run lint:fix
 
 echo 'Remove build folder to make sure the npm package only includes clean built components'
 if [ -d "./build" ]; then rm -rf ./build; fi # 
-# npm run build
+npm run build
 tsc --declaration 
 
 
